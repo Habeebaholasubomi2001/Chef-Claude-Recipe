4bbd37cfb9d22ee4dabb0feab37690fd65702670
Habeebaholasubomi2001 <sulaymanhabibat2001@gmail.com>
2024-12-24 11:48:38 -0700

    made some changes

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ChefClaude-Recipe-App",
+  base: "/Chef-Claude-Recipe",
   build: {
     rollupOptions: {
-      external: ["@huggingface/inference"],
+      external: ["@huggingface/inference", "react-markdown"],
       dependencies: {
         "@huggingface/inference": "^2.8.1",
         "gh-pages": "^6.2.0",
