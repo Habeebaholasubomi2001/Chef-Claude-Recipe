3519ce8abdb7ce01e897a2e650bd85950d132284
Habeebaholasubomi2001 <sulaymanhabibat2001@gmail.com>
2024-12-28 02:14:36 -0700

    Configure for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,23 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Chef-Claude-Recipe",
-  build: {
-    rollupOptions: {
-      external: ["@huggingface/inference", "react-markdown"],
-      dependencies: {
-        "@huggingface/inference": "^2.8.1",
-        "gh-pages": "^6.2.0",
-        "react-markdown": "^9.0.1",
-      },
-    },
-  },
+  base: "/",
 });
-
-// export default {
-//   build: {
-//     rollupOptions: {
-//       external: ['@huggingface/inference']
-//     }
-//   }
-// }
